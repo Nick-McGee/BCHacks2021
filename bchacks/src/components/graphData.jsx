1b042feda1ea5b14901de3ef635265835fc305e4
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2021-02-20 23:43:47 -0700

    Added weekly high low

--- bchacks/src/components/graphData.jsx
+++ bchacks/src/components/graphData.jsx
@@ -7,19 +7,33 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import NewsCol from './newscol';
 import { GetNews } from '../news-service';
+import { getLowHigh } from '../minLow-service';
 
-  const chartStyle = {
-    width: '90%',
-      height: '400px',
-      margin: '0px auto'
-  };
+const nameLineup = {
+  paddingLeft: '2%',
+  fontSize: '24px'
+}
+
+const statsLineup = {
+  paddingTop: '1.5%'
+}
+
+const chartStyle = {
+  width: '90%',
+    height: '400px',
+    margin: '0px auto'
+};
 
 const GraphData = ({stockClass, ticker, companyname, highvalue, lowvalue, sentimentcount, graphdatarray}) => {
     //Use ticker to retrieve the appropriate news
 
     const [news, setNews] = useState([]);
-    if (news.length == 0) GetNews(ticker).then(result => setNews(result));
+    if(news.length === 0) GetNews(ticker).then(result => setNews(result));
     if(news.length > 0) console.log(news[0][0]);
+
+    const [minMax, setMaxMin] = useState([]);
+    if(minMax.length === 0) getLowHigh(ticker).then(result => setMaxMin(result));
+    if(minMax.length > 0) console.log(minMax);
 
     return (
         <Container>
@@ -29,12 +43,12 @@ const GraphData = ({stockClass, ticker, companyname, highvalue, lowvalue, sentim
             <Row>
               <Col sm={5}>
                 <h1 class={stockClass} id="ticker">{ticker}</h1>
-                <p>{companyname}</p>
+                <p style={nameLineup}>{companyname}</p>
               </Col>
-              <Col sm={5}>
-                <p><FaAngleUp />Weekly High: <span class="good" id="high-value">{highvalue}</span></p>
-                <p><FaAngleDown />Weekly Low:  <span class="bad" id="low-value">{lowvalue}</span></p>
-                <p><FaMale />Sentiment Count: <span>{sentimentcount}</span></p>
+              <Col sm={5} style={statsLineup}>
+                <p><FaAngleUp /> Weekly High: <span class="good" id="high-value">{minMax[1]} USD</span></p>
+                <p><FaAngleDown /> Weekly Low:  <span class="bad" id="low-value">{minMax[0]} USD</span></p>
+                <p><FaMale /> Sentiment Count: <span>{sentimentcount}</span></p>
               </Col>
             </Row>
             <Row>
---
+++ bchacks/src/minLow-service.js
@@ -0,0 +1,42 @@
+export const getLowHigh = (ticker) => {
+    var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker + '&apikey=46C5ZW2US27WT7FY'
+
+    return fetch(url)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
+            return response.json()
+        })
+        .then(json => {
+            let date = '';
+            let max = 0;
+            let min = 1000000;
+
+            for(let i = 0; i < 7; i++) {
+                var today = new Date();
+                today.setDate(today.getDate() - i);
+                var dd = today.getDate();
+                var mm = today.getMonth()+1; 
+                var yyyy = today.getFullYear();
+                if(dd<10) {dd='0'+dd;} 
+                if(mm<10){mm='0'+mm;}
+                date = yyyy+'-'+mm+'-'+dd;
+
+                try {
+                    max = Math.max(max, json['Time Series (Daily)'][date]['2. high']);
+                    min = Math.min(min, json['Time Series (Daily)'][date]['3. low']);
+                }
+                catch (err){
+                    // console.log("market closed on " + date);
+                }
+            }
+
+            let minMax = [min, max];
+            return minMax;
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
