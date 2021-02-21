6cf2f01b0bf5388a3f88cf8be7f431ef8cf2827f
MoatazAR <43458713+MoatazAR@users.noreply.github.com>
2021-02-21 04:11:19 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
 import Header from './components/header';
 import GraphData from './components/graphData';
 import './App.css';
 import WsbImage from './images/wsb.jpeg';
 import 'bootstrap/dist/css/bootstrap.css';
 import { GraphDataArray } from './graph';
-import { NewsArray } from './news';
-import { GetNews } from './news-service';
 
-const newarray = GetNews('GME');
-
-console.log(newarray[0]);
-
-const stockArray= [
+ /*const stockArray= [
   {  
   stockClass: 'good',
   ticker: 'GME',
@@ -21,25 +14,50 @@ const stockArray= [
   highvalue: '53.37 USD',
   lowvalue: '38.60 USD',
   sentimentcount: '7,234',
-  graphdatarray: GraphDataArray,
-  newsarray: NewsArray
+  graphdatarray: GraphDataArray
   }
-]
+]*/
 
+class App extends Component {
+  state = { 
+    stockArray: [
+      {  
+      stockClass: 'good',
+      ticker: 'GME',
+      companyname: 'Gamestop Inc',
+      highvalue: '53.37 USD',
+      lowvalue: '38.60 USD',
+      sentimentcount: '7,234',
+      graphdatarray: GraphDataArray
+      }
+    ]    
+  }
 
-function App() {
-  return (
-    <div className="main">
+  /*async componentDidMount() {
+    const url= "https://api.randomuser.me/";
+    const response = await fetch(url);
+    this.setState({stockArray: response});
+  }*/
+
+  render() {
+  
+    const { stockArray } = this.state
+    return (
+      <div className="main">
       <Header
         header="Wall Street Bets Sentimental Analysis" 
         parag="Using sentimental analysis, we analyze what stocks the users on r/wallstreetbets are talking about"
         imgsrc={WsbImage}
       />
-      {stockArray.map(stock => <GraphData stockClass={stock.stockClass}  ticker={stock.ticker} companyname={stock.companyname} highvalue={stock.highvalue} lowvalue={stock.lowvalue} sentimentcount={stock.sentimentcount} graphdatarray={stock.graphdatarray} newsarray={stock.newsarray}/>)}
-      
+      {//stockArray.map(stock => <GraphData stockClass={stock.stockClass}  ticker={stock.ticker} companyname={stock.companyname} highvalue={stock.highvalue} lowvalue={stock.lowvalue} sentimentcount={stock.sentimentcount} graphdatarray={stock.graphdatarray}/>)}
+      }
+      {stockArray.map(stock => <GraphData stockClass={stock.stockClass}  ticker={stock.ticker} companyname={stock.companyname} highvalue={stock.highvalue} lowvalue={stock.lowvalue} sentimentcount={stock.sentimentcount} graphdatarray={stock.graphdatarray}/>)}
     </div>
-  );
+    );
+  }
 }
+
+
 // stockClass is used for the class that will decide whether a stock is doing well or not by changing the color to either green or red
 
 export default App;
